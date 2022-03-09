67cb665d8f2c44f17870cc1d8f5c20fd83165afe
ddmytruk@s-pro.io <23071996>
2022-03-09 04:27:10 +0200

    Ftfg

--- katya/settings.py
+++ katya/settings.py
@@ -15,11 +15,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    'lit-ravine-51997.herokuapp.com', '127.0.0.1'
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
